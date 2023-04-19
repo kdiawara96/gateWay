df1f0d4117c7b0aba8162d4fbd93068a3f66d58b
Diawara Karim <karimdiawara96@gmail.com>
2023-04-19 13:47:34 +0000

    create les models dans model customer and product

---
+++ billing-service/src/main/java/com/ml/billingservice/models/Customer.java
@@ -0,0 +1,13 @@
+package main.java.com.ml.billingservice.models;
+
+
+@data   
+@AllArgsConstructor
+@NoargsConstructor
+
+public class Customer {
+    private Long id;
+    private String name;
+    private String email;
+    
+}
---
+++ billing-service/src/main/java/com/ml/billingservice/models/Product.java
@@ -0,0 +1,12 @@
+package main.java.com.ml.billingservice.models;
+
+@data 
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+private Long id;
+private String name;
+private double price;
+private int quantity;    
+}
